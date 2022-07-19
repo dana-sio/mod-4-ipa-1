6002a869654ab73f09a0ca4ee8bbfe370aac73c0
Dana Sio <90906586+dana-sio@users.noreply.github.com>
2022-07-19 12:15:24 +0800

    Update mod-4-ipa-1.py

--- mod-4-ipa-1.py
+++ mod-4-ipa-1.py
@@ -64,14 +64,35 @@ def tic_tac_toe(board):
     '''
     # Replace `pass` with your code. 
     # Stay within the function. Only use the parameters as input. The function should return your answer.
+def tic_tac_toe(board):
+    '''Tic Tac Toe. 
+    25 points.
+    Tic Tac Toe is a common paper-and-pencil game. 
+    Players must attempt to successfully draw a straight line of their symbol across a grid.
+    The player that does this first is considered the winner.
+    This function evaluates a tic tac toe board and returns the winner.
+    Please see "assignment-4-sample-data.py" for sample data. The board will adhere
+    to the same pattern. The board may by 3x3, 4x4, 5x5, or 6x6. The board will never
+    have more than one winner. The board will only ever have 2 unique symbols at the same time.
+    Parameters
+    ----------
+    board: list
+        the representation of the tic-tac-toe board as a square list of lists
+    Returns
+    -------
+    str
+        the symbol of the winner or "NO WINNER" if there is no winner
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
     horizontal_check = [x for x in board]
     vertical_check = [x for x in zip(*board)]
     updown_diagonal_check = [board[i][i] for i,v in enumerate(board)]
     downup_diagonal_check = [board[len(board)-1-i][i] for i,v in enumerate(board)]
     
-    if any(z == ["X"]*len(board) for z in horizontal_line) or any(z == tuple(["X"]*len(board)) for z in vertical_line) or all(z == "X" for z in updown_diagonal_line) or all(z == "X" for z in downup_diagonal_line):
+    if any(z == ["X"]*len(board) for z in horizontal_check) or any(z == tuple(["X"]*len(board)) for z in vertical_check) or all(z == "X" for z in updown_diagonal_check) or all(z == "X" for z in downup_diagonal_check):
         return "X"
-    elif any(z == ["O"]*len(board) for z in horizontal_line) or any(z == tuple(["O"]*len(board)) for z in vertical_line) or all(z == "O" for z in updown_diagonal_line) or all(z == "O" for z in downup_diagonal_line):
+    elif any(z == ["O"]*len(board) for z in horizontal_check) or any(z == tuple(["O"]*len(board)) for z in vertical_check) or all(z == "O" for z in updown_diagonal_check) or all(z == "O" for z in downup_diagonal_check):
         return "O"
     return None
 
